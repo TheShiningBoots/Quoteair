22c058ea3ff14fe419425e7886713bebb872279d
TheShiningBoots <theshiningboots@gmail.com>
2022-04-26 18:53:33 +0530

    Fix a bug and add comments

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,7 @@
 const quoteair = require("../src/quoteair");
 
+// Outputs the quotes as json with the author
 console.log(quoteair.getRandomQoute());
 
+// Outputs the quote as text without the author
 console.log(quoteair.getRandomQoute(false))
