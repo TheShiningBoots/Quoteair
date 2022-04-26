b0feeac595ae41de9272958733a02a000d2e324d
TheShiningBoots <theshiningboots@gmail.com>
2022-04-26 17:59:23 +0530

    Initial Commit

---
+++ examples/example.js
@@ -0,0 +1,5 @@
+const quoteair = require("../src/quoteair");
+
+console.log(quoteair.getRandomQoute());
+
+console.log(quoteair.getRandomQoute(false))
---
+++ src/quoteair.js
@@ -0,0 +1,19 @@
+const data = require('../db/quotes.json')
+
+let processedQuote = {};
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  module.exports.getRandomQoute = function (type) {
+    let index = randomInt(0, data.length);
+    switch (type) {
+      case false:
+        return data[index].quote;
+      default:
+        processedQuote.quote = data[index].quote;
+        processedQuote.author = data[index].author;
+        return processedQuote;
+    }
+  };
